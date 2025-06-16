4e1c60aa06b00e889d5a885b0399ddccc9251e22
Zack <zacharycollier36@protonmail.com>
2025-06-16 19:35:02 -0230

    Added attributes and constructors for TaskList.java and User.java
    - Also started on Main.java, initializing a user array
    - - Judging from the prompt, an ArrayList may work better

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
-
+/*
+1. Creates users and adds them to an array. (this seems more suited to an arraylist tbh)
+2. Adds tasks to users' to-do lists.
+3. Marks tasks as completed.
+4. Prints all tasks for each user.
+*/
 public class Main {
     public static void main(String[] args) {
-
+        User[] userArray = new User[3];
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,14 @@
 package org.example;
 
+
+//Includes methods to add a task, mark a task as completed, and print all tasks.
+
 public class User {
+    private String userName;
+    private TaskList taskList;
+
+    public User(String userName, Task firstTask){
+        this.userName = userName;
+        this.taskList = new TaskList(firstTask);
+    }
 }
