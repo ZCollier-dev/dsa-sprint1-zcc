1fa1aa9811e922a36af8c33617490b05bca9a68d
Zack <zacharycollier36@protonmail.com>
2025-06-16 19:31:37 -0230

    Updated Task.java with getters, setters (including for the isDone attribute), and toString method

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,4 +1,36 @@
 package org.example;
 
 public class Task {
+    private String description;
+    private boolean isDone = false;
+
+    public Task(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+    public boolean getDoneStatus(){
+        return this.isDone;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setDoneStatus(boolean isDone){
+        this.isDone = isDone;
+    }
+
+    public String toString(){
+        String returnString = this.description + ": ";
+
+        if (this.isDone){
+            returnString += "Done! :)";
+        } else {
+            returnString += "Not Done.";
+        }
+
+        return returnString;
+    }
 }
